9af5dd8ba2752e5d8ea9c7c640f05c0731bde57b
christinen22 <christine.elin.louise@gmail.com>
2023-09-02 10:54:54 +0200

    React BS and state for modal

--- src/components/EditTodo.tsx
+++ src/components/EditTodo.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
 
 const EditTodo = () => {
-  return <div></div>;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const handleShow = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Edit Todo
+      </Button>
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Todo Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Control type="text" placeholder="Todo Details" />
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Update Todo
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default EditTodo;
